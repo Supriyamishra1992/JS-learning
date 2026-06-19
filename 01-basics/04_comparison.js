e7f598eef00aed69b0bc5ae6266e91e47d395c94
Supriyamishra1992 <51369118+Supriyamishra1992@users.noreply.github.com>
2026-06-19 11:34:59 +0000

    comparison

---
+++ 01-basics/04_comparison.js
@@ -0,0 +1,20 @@
+// console.log(2 > 3)
+// console.log(2 >= 3)
+// console.log(2 < 3)
+// console.log(2 <= 3)
+// console.log(2 == 3)
+// console.log(2 != 3)
+
+// console.log("2" > 3)
+// console.log("02" > 3)
+
+console.log(null > 0)
+console.log(null < 0)
+console.log(null >= 0)
+
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined >= 0)
+
+// === strict check => checks value and data type too
+console.log("2" > 2);
