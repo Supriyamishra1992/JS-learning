2fa18c1b5a2084c0c77f4b6e12c82b47ed7c5b6d
Supriyamishra1992 <51369118+Supriyamishra1992@users.noreply.github.com>
2026-06-19 10:18:57 +0000

    datatype

---
+++ 01-basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; // treat the code newer version
+//alert(3 +3) // not allowed because we are using nodeJS, it can run in browser
+//ecmas(tc39) & mdm
+
+// number
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => when varible declare but no value assigned
+// symbol => unique
+
+// object
+
+console.log(typeof 34);
+console.log(typeof "supriya");
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
